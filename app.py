36893750936ec0c7548c30f4279bd42858fa00fc
Robert-Ecke83 <56957502+Robert-Ecke83@users.noreply.github.com>
2020-04-09 11:57:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import csv
 app = Flask(__name__)
 
 
-BOT_TOKEN = 'NzAxYjYzNzItZmM4MS00MGVmLTgyMzEtYTUzMDk5YWY2Zjk4ZGQ3ZWI1MmEtZjg1_PE93_e9ed1911-dd0d-4e14-a181-b40037bebc6b'
+BOT_TOKEN = 'ZTU0YTQ0Y2EtZTU3Ny00YTk3LTkyMjItYjQwNWRjZjc2MTI0YzhjMzEyNmMtN2Iw_PE93_e9ed1911-dd0d-4e14-a181-b40037bebc6b'
 
 api = CiscoSparkAPI(access_token=BOT_TOKEN)
 
